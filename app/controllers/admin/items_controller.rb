3cec760757153dbce17e09ea6dc58cb7c50a4965
toshikioi0601 <toshikioi0601@gmail.com>
2021-03-18 04:48:46 +0000

    [Add] items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,25 @@
 class Admin::ItemsController < ApplicationController
   def index
+
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(genre_params)
+    # @image = Image.new(genre_params)
+    if @item.save
+      # @image.save
+      redirect_to admin_item_path, notice: "You have created item successfully."
+    else
+      @item = Item.new
+      # @image = Image.new
+      render :new
+    end
   end
+
 
   def show
   end
@@ -15,5 +28,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+
+  end
+
+  private
+
+  def image_params
+    params.require(:item).permit(:image)
   end
 end
