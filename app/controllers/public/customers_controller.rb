6f21cb49eca93b90596ca8403cbadfa5705f7534
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-03-19 12:37:23 +0000

    [Fix]マイページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,5 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
     @customer = current_customer
   end
@@ -15,7 +15,7 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
-  
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
