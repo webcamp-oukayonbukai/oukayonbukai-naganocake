6065148bc8a811424f492e3e904da215d3e53621
toshikioi0601 <ec2-user@ip-172-31-90-23.ec2.internal>
2021-03-20 02:40:52 +0000

    [Add]item_cart]

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,11 @@
 class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
+  
+  validates :item_id, presence: true, uniqueness: { scope: :customer_id }
+  #ネット参照必要かわからない(https://qiita.com/j-sunaga/items/d7f0e944baad6e56206c)
+  
+  def subtotal
+		item.with_tax_price * amount
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :genre
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy #(https://qiita.com/Tsh-43879562/items/fbc968453a7063776637)
   has_many :order_details
   
   attachment :image
