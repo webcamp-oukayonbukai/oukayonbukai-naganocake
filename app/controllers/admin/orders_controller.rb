63e410dfb7cec0ba523868ca41b1da4d4a0ca84e
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-03-21 02:23:04 +0000

    [Add]管理者注文履歴詳細画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,21 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.all
   end
 
   def update
+    @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_detail_params)
+      redirect_to admin_order_path(@order)
+    else
+      render :show
+    end
+  end
+  
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:quantity, :make_status, :price)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   has_many :order_details
   belongs_to :customer
   
+  def item_price_total
+    Item.all.sum(:subtotal)
+  end
+  
+  def total_price
+    
+  end
+  
 end
