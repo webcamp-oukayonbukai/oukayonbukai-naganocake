039108629b06c980d78ad836358f7180cd38ad0c
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-03-21 00:17:57 +0000

    [Add]管理者注文一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,6 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @orders = Order.page(params[:page]).per(10)
   end
 
   def update
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   has_many :order_details
   belongs_to :customer
   
+  enum buy_status: { wait: 0, check: 1, making: 2, prepare: 3, sent: 4 }
+  
 end
